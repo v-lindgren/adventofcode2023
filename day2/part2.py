04c8a01aa370e1ed2976e5516533f2b054bfc21d
v-lindgren <viktor.lindgren+vlindgren@gmail.com>
2023-12-02 11:51:03 +0100

    solved day2part2

---
+++ day2/part2.py
@@ -0,0 +1,28 @@
+import re
+import sys
+sys.path.append('../')
+from parse_input import parse_input
+
+input_lines = parse_input('input.txt', 'list')
+
+sum_powers = 0
+
+for line in input_lines:
+    # find all '(count) (colour)' pairs
+    matches = re.findall('[,;:] ([0-9]*) (\w+)', line)
+
+    # sort by the the count (default sort sorts as string)
+    matches.sort(key = lambda x:int(x[0]))
+
+    # as we have sorted the sets from small to large, we can do this the
+    # lazy way and just overwrite to end up with the largest value
+    colour_count = { set[1]: int(set[0]) for set in matches if set[0] }
+
+    # multiply the values together, and add to sum
+    power = 1
+    for count in colour_count.values():
+        power *= int(count)
+
+    sum_powers += power
+
+print(sum_powers)
