23f01dc54cc63c45444fdad077b8c13803c0c301
v-lindgren <viktor.lindgren+vlindgren@gmail.com>
2023-12-03 14:31:52 +0100

    start of day3part2

---
+++ day3/part2.py
@@ -0,0 +1,58 @@
+import re
+from functools import reduce
+import sys
+sys.path.append('../')
+from parse_input import parse_input
+
+input_lines = parse_input('input.txt', 'list')
+PART_SYMBOLS = '[*]'
+part_numbers = []
+
+def get_active_positions(line):
+    positions = []
+    for match in re.finditer(PART_SYMBOLS, line):
+        positions.append(match.start())
+
+    return positions
+
+def get_part_number_positions(line):
+    positions =[]
+    for match in re.finditer('[0-9]+', line):
+        positions.append([match.group(), match.start(), match.end()])
+
+    return positions
+
+# We're looking at 3 lines at a time to check adjacency
+numbers_previous = []
+numbers_current = []
+numbers_next = []
+
+for row_number, line in enumerate(input_lines):
+    # find all numbers' locations in prev, current & next line
+    numbers_current = numbers_next if numbers_next else get_part_number_positions(line)
+    numbers_next = get_part_number_positions(input_lines[row_number + 1]) if row_number + 1 < len(input_lines) else []
+    
+    gear_positions = get_active_positions(line)
+
+    active_pos = []
+    if gear_positions:
+        
+        # create a sorted unique set of all nearby numbers' positions
+        num_positions = numbers_previous + numbers_current + numbers_next
+        
+        # TODO - method below doesn't work - have to check each number individually for each gear
+        #for num in num_positions:
+        #    active_pos += list(range(num[1]-1, num[2]+1))
+        #active_pos = set(active_pos)
+
+        for gear in gear_positions:
+
+
+        part_number_positions = []
+        for pos in num_positions:
+            part_number_positions += list(range(pos[1]-1, pos[2]+2))
+
+
+    numbers_previous = numbers_current
+
+#print(sum([eval(num) for num in part_numbers]))
