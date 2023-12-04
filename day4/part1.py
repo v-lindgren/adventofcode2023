0a166f1cf6f4f3e1e3666d1bdce19e953242df8f
v-lindgren <viktor.lindgren+vlindgren@gmail.com>
2023-12-04 11:23:08 +0100

    day4part1 solved

--- day4/part1.py
+++ day4/part1.py
@@ -5,16 +5,14 @@ from parse_input import parse_input
 
 input_lines = parse_input('input.txt', 'list')
 
-def get_numbers(numberlist):
-    return [ int(y) for y in re.findall('[0-9]+', numberlist)]
-
 all_winning_numbers = []
 
 for line in input_lines:
-    winning_numbers, my_numbers = map(get_numbers, re.split(':|\| +', line)[1::])
+    card = re.split(':|\| +', line)[1::]
+    winning_numbers, my_numbers = map(lambda x: [ int(num) for num in re.findall('[0-9]+', x)], card)
 
-    my_winning_numbers = (set(winning_numbers) & set(my_numbers)) or {}
+    my_winning_numbers = (set(winning_numbers) & set(my_numbers))
     all_winning_numbers.append(my_winning_numbers)
 
-score = [ int(2 ** (len(x) - 1)) for x in all_winning_numbers]
+score = [ int(2 ** (len(x) - 1)) for x in all_winning_numbers ]
 print(sum(score))
